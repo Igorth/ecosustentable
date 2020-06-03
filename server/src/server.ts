528d623bcb26512ddb460f699a548ce21bc3d627
Igor Dias <igordiasth@gmail.com>
2020-06-03 15:02:32 +1200

    editorcondig, eslint, prettier

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  return (response.json({'message': 'users'}))
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
