786fb572cc3e6edc2add52932246ea677f535b7d
Igor Dias <igordiasth@gmail.com>
2020-06-05 14:35:23 +1200

    search items from api

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+
+import api from '../../services/api';
 
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg'
 
+interface ItemsProps {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<ItemsProps[]>([]);
+
+  useEffect(() => {
+    api.get('items')
+      .then(response => {
+        setItems(response.data);
+      })
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -96,35 +113,12 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="lamps"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="lamps"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="lamps"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="lamps"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="lamps"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="lamps"/>
-              <span>Óleo de Cozinha</span>
-            </li>
+            {items.map(item => (
+              <li key={item.id}>
+                <img src={item.image_url} alt={item.title}/>
+                <span>{item.title}</span>
+              </li>  
+            ))}          
           </ul>
         </fieldset>
 
