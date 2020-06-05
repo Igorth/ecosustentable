d4f52cf4d0bc3c78928b801694cfd085014f9d0f
Igor Dias <igordiasth@gmail.com>
2020-06-05 16:29:16 +1200

    finished app for web

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState, ChangeEvent} from 'react';
-import { Link } from 'react-router-dom';
+import React, {useEffect, useState, ChangeEvent, FormEvent} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
@@ -23,7 +23,7 @@ interface IBGEUFResponse {
 }
 
 interface IBGECityResponse {
-  name: string;
+  nome: string;
 }
 
 const CreatePoint = () => {
@@ -33,9 +33,18 @@ const CreatePoint = () => {
 
   const[initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
 
+  const[formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: ''
+  });
+
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+  const history = useHistory();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -71,7 +80,7 @@ const CreatePoint = () => {
 
     axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
     .then(response => {
-      const cityNames = response.data.map(city => city.name);
+      const cityNames = response.data.map(city => city.nome);
 
       setCities(cityNames);
     })
@@ -97,6 +106,52 @@ const CreatePoint = () => {
     ])
   }
 
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if(alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      setSelectedItems(filteredItems);
+
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }    
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const {name, value} = event.target;
+
+    setFormData({...formData, [name]: value});
+  }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items
+    }
+
+    api.post('points', data);
+
+    alert('Ponto de coleta criado');
+
+    history.push('/');
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -108,7 +163,7 @@ const CreatePoint = () => {
         </Link>
       </header>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
 
         <fieldset>
@@ -122,6 +177,7 @@ const CreatePoint = () => {
             type="text"
             name="name"
             id="name"
+            onChange={handleInputChange}
             />
           </div>
 
@@ -132,6 +188,7 @@ const CreatePoint = () => {
             type="email"
             name="email"
             id="email"
+            onChange={handleInputChange}
             />
           </div>
 
@@ -141,6 +198,7 @@ const CreatePoint = () => {
             type="text"
             name="whatsapp"
             id="whatsapp"
+            onChange={handleInputChange}
             />
           </div>
           </div>
@@ -191,7 +249,11 @@ const CreatePoint = () => {
 
           <ul className="items-grid">
             {items.map(item => (
-              <li key={item.id}>
+              <li 
+              key={item.id} 
+              onClick={() => handleSelectItem(item.id)}
+              className={selectedItems.includes(item.id) ? 'selected' : ''}
+              >
                 <img src={item.image_url} alt={item.title}/>
                 <span>{item.title}</span>
               </li>  
